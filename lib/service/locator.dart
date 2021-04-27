77f2947851721ec84998c5efff44f361762f85a6
akramAssi <55202260+akramAssi@users.noreply.github.com>
2021-04-27 03:32:27 +0300

    add FirestoreDatabase to Singleton

--- lib/service/locator.dart
+++ lib/service/locator.dart
@@ -1,5 +1,6 @@
 import 'package:get_it/get_it.dart';
 import 'package:souqy/service/Auth.dart';
+import 'package:souqy/service/database_repo.dart';
 import 'package:souqy/service/storage_repo.dart';
 import 'package:souqy/view_controller/user_controller.dart';
 
@@ -8,5 +9,6 @@ final locator = GetIt.instance;
 void setupServices() {
   locator.registerSingleton<Auth>(Auth());
   locator.registerSingleton<StorageRepo>(StorageRepo());
+  locator.registerSingleton<FirestoreDatabase>(FirestoreDatabase());
   locator.registerSingleton<UserController>(UserController());
 }
