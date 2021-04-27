e11ffc9a10ab3989f89e7cb3ac2adf5e1fbce2a6
akramAssi <55202260+akramAssi@users.noreply.github.com>
2021-04-27 03:37:42 +0300

    create database_repo

--- lib/service/database_repo.dart
+++ lib/service/database_repo.dart
@@ -1,17 +1,35 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:souqy/model/user_model.dart';
 
-abstract class Database {}
+abstract class Database {
+  Future<void> storeUserInfo(UserModel user);
+  void readUserInfo(UserModel user);
+}
 
 class FirestoreDatabase implements Database {
-  FirestoreDatabase({@required this.uid});
-  final String uid;
+  // FirestoreDatabase({@required this.uid});
+  // final String uid;
   final _firestore = FirebaseFirestore.instance;
 
   Future<void> storeUserInfo(UserModel user) async {
-    String path = "/user/${user.uid}";
-    final firestore_ref = _firestore.doc(path);
-    await firestore_ref.set(user.address);
+    final String path = "/user/${user.uid}";
+    final firestoreRef = _firestore.doc(path);
+    await firestoreRef.set(user.address);
+  }
+
+  void readUserInfo(UserModel user) {
+    final String path = "/user/${user.uid}";
+    final firestoreRef = _firestore.doc(path);
+    final snapshot = firestoreRef.snapshots();
+    print("dsdadasdas");
+    snapshot.map(
+      // (snapshot) => snapshot.docs.map(
+      (snapshot) {
+        final data = snapshot.data();
+        user.setAddress(
+            phone: data["phone"], city: data["city"], area: data["area"]);
+      },
+      // ),
+    );
   }
 }
