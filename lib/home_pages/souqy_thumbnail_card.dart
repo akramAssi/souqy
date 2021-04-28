279ccab8cdd22fe903a667c850f5a5472684abe6
akramAssi <55202260+akramAssi@users.noreply.github.com>
2021-04-28 23:35:53 +0300

    create widget SouqyThumbnailCard

---
+++ lib/home_pages/souqy_thumbnail_card.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SouqyThumbnailCard extends StatelessWidget {
+  final String path;
+  SouqyThumbnailCard({Key key, this.path}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(10),
+      height: 135,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        image: DecorationImage(
+          fit: BoxFit.fill,
+          image: AssetImage(path),
+          // image: AssetImage('lib/mo/kia-op.jpg'),
+        ),
+      ),
+    );
+  }
+}
