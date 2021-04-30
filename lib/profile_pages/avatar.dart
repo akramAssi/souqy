56e8a2ada9452ec894f8df4e4a273b90c0837a92
akramAssi <55202260+akramAssi@users.noreply.github.com>
2021-04-30 11:31:03 +0300

    add flag setting

--- lib/profile_pages/avatar.dart
+++ lib/profile_pages/avatar.dart
@@ -3,15 +3,78 @@ import 'package:souqy/res/color.dart';
 
 class Avatar extends StatelessWidget {
   final String avatarUrl;
+  final bool isSetting;
   final VoidCallback onPress;
-  const Avatar({
+  Avatar({
     Key key,
     this.avatarUrl,
     @required this.onPress,
+    this.isSetting,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    var widget = [
+      avatarUrl == null
+          ? Image.asset(
+              "images/big_man.png",
+              height: 230,
+              width: 230,
+            )
+          : Container(
+              height: 230.0,
+              width: 230.0,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: NetworkImage(
+                    avatarUrl,
+                  ),
+                ),
+              ),
+            ),
+      Positioned(
+        right: 4,
+        top: 42,
+        child: SizedBox(
+          width: 73,
+          height: 21,
+          child: ElevatedButton(
+            onPressed: () {
+              print("sd");
+            },
+            child: Text(
+              "ترقيه",
+              style: TextStyle(color: primeCOLOR, fontSize: 16),
+            ),
+            style: ElevatedButton.styleFrom(
+              primary: annLable,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    bottomLeft: Radius.circular(10)),
+              ),
+            ),
+          ),
+        ),
+      ),
+    ];
+    if (isSetting) {
+      widget.add(
+        Positioned(
+          right: 32,
+          bottom: 0,
+          child: IconButton(
+            onPressed: onPress,
+            icon: Icon(
+              Icons.add,
+              size: 45,
+            ),
+          ),
+        ),
+      );
+    }
     return Container(
       margin: EdgeInsets.only(top: 22),
       height: 230,
@@ -20,63 +83,7 @@ class Avatar extends StatelessWidget {
         border: Border.all(),
       ),
       child: Stack(
-        children: [
-          avatarUrl == null
-              ? Image.asset(
-                  "images/big_man.png",
-                  height: 230,
-                  width: 230,
-                )
-              : Container(
-                  height: 230.0,
-                  width: 230.0,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                      fit: BoxFit.fill,
-                      image: NetworkImage(
-                        avatarUrl,
-                      ),
-                    ),
-                  ),
-                ),
-          Positioned(
-            right: 4,
-            top: 42,
-            child: SizedBox(
-              width: 73,
-              height: 21,
-              child: ElevatedButton(
-                onPressed: () {
-                  print("sd");
-                },
-                child: Text(
-                  "ترقيه",
-                  style: TextStyle(color: primeCOLOR, fontSize: 16),
-                ),
-                style: ElevatedButton.styleFrom(
-                  primary: annLable,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(10),
-                        bottomLeft: Radius.circular(10)),
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Positioned(
-            right: 32,
-            bottom: 0,
-            child: IconButton(
-              onPressed: onPress,
-              icon: Icon(
-                Icons.add,
-                size: 45,
-              ),
-            ),
-          ),
-        ],
+        children: widget,
       ),
     );
   }
