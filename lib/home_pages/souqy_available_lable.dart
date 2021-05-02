7ec652440be9c682e6ee6cdde1e2920fb1a984e8
akrma Assi <akram.20002010@gmail.com>
2021-05-02 11:44:17 +0300

    remove Positioned widget

--- lib/home_pages/souqy_available_lable.dart
+++ lib/home_pages/souqy_available_lable.dart
@@ -2,36 +2,40 @@ import 'package:flutter/material.dart';
 import 'package:souqy/res/color.dart';
 
 class SouqyAvailableLable extends StatelessWidget {
-  const SouqyAvailableLable({
+  SouqyAvailableLable({
     Key key,
     @required this.available,
     @required this.size,
+    @required this.isCard,
   }) : super(key: key);
 
   final Size size;
   final bool available;
+  final bool isCard;
 
   @override
   Widget build(BuildContext context) {
-    return Positioned(
-      right: size.width / 80,
-      top: 25.0,
-      child: Container(
-        width: 60,
-        height: 23,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(10), bottomLeft: Radius.circular(10)),
-          color: available == true ? annLable : redColor,
+    // BorderRadiusGeometry radius =
+
+    return Container(
+      width: 60,
+      height: 23,
+      decoration: BoxDecoration(
+        borderRadius: isCard == true
+            ? BorderRadius.only(
+                topLeft: Radius.circular(10), bottomLeft: Radius.circular(10))
+            : BorderRadius.only(
+                topRight: Radius.circular(10),
+                bottomRight: Radius.circular(10)),
+        color: available == true ? annLable : redColor,
+      ),
+      child: Text(
+        available == true ? 'متوفر' : "مباع",
+        style: TextStyle(
+          fontSize: 13,
+          color: available == true ? primeCOLOR : Colors.white,
         ),
-        child: Text(
-          available == true ? 'متوفر' : "مباع",
-          style: TextStyle(
-            fontSize: 13,
-            color: available == true ? primeCOLOR : Colors.white,
-          ),
-          textAlign: TextAlign.center,
-        ),
+        textAlign: TextAlign.center,
       ),
     );
   }
