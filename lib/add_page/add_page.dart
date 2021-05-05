59c7c277601e90a6dd82cf618260e003e4435049
akramAssi <55202260+akramAssi@users.noreply.github.com>
2021-05-06 01:11:03 +0300

    add year  and model field

--- lib/add_page/add_page.dart
+++ lib/add_page/add_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:souqy/add_page/search_brand.dart';
-import 'package:souqy/res/color.dart';
+import 'package:souqy/widget/souqy_TextFiled.dart';
 
 import 'car_type.dart';
 
@@ -10,16 +10,46 @@ class AddPage extends StatefulWidget {
 }
 
 class _AddPageState extends State<AddPage> {
+  TextEditingController _yearController;
+  TextEditingController _modelController;
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return Directionality(
-        textDirection: TextDirection.rtl,
-        child: ListView(
-          children: [
-            SearchForPrand(),
-            CarType(),
-          ],
-        ));
+    return ListView(
+      children: [
+        SearchForPrand(),
+        CarType(),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            children: [
+              Flexible(
+                flex: 1,
+                child: SouqyTextField(
+                  lable: "Year",
+                  controller: _yearController,
+                  height: 50,
+                  isReadOnly: true,
+                ),
+              ),
+              SizedBox(
+                width: size.width * .03,
+              ),
+              Flexible(
+                flex: 3,
+                child: InkWell(
+                  child: SouqyTextField(
+                    lable: "Model",
+                    controller: _modelController,
+                    height: 50,
+                  ),
+                  onTap: () {},
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
