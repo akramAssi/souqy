c1cc92da7777bbec850c6e7b0bfc2be643a1086f
akramAssi <55202260+akramAssi@users.noreply.github.com>
2021-05-08 01:37:53 +0300

    handle image from file  & remove image url

--- lib/moreInfoPage/souqy_imge_slider.dart
+++ lib/moreInfoPage/souqy_imge_slider.dart
@@ -1,25 +1,121 @@
+import 'dart:io';
+
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:souqy/res/color.dart';
 
-class CarouselWithIndicatorDemo extends StatefulWidget {
+// ignore: must_be_immutable
+class SouqyImageSlider extends StatefulWidget {
+  List<String> imageList;
+  final String source;
+  final bool autoPlay;
+  final void Function(String) onLongPress;
+  SouqyImageSlider(
+      {Key key,
+      @required this.imageList,
+      @required this.source,
+      this.autoPlay = true,
+      this.onLongPress})
+      : super(key: key);
   @override
   State<StatefulWidget> createState() {
     return _CarouselWithIndicatorState();
   }
 }
 
-class _CarouselWithIndicatorState extends State<CarouselWithIndicatorDemo> {
+class _CarouselWithIndicatorState extends State<SouqyImageSlider> {
   int _current = 0;
+  List<Widget> imageSliders;
 
   @override
   Widget build(BuildContext context) {
+    switch (widget.source) {
+      case "Network":
+        {
+          imageSliders = widget.imageList
+              .map((item) => Container(
+                    child: Container(
+                      margin: EdgeInsets.only(
+                          top: 7.0, right: 7.0, left: 7.0, bottom: 15.0),
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.all(Radius.circular(35.0)),
+                          child: Stack(
+                            children: <Widget>[
+                              Image.network(item,
+                                  fit: BoxFit.cover, width: 1000.0),
+                            ],
+                          )),
+                    ),
+                  ))
+              .toList();
+          break;
+        }
+
+      case "File":
+        {
+          imageSliders = widget.imageList
+              .map((item) => Container(
+                    child: Container(
+                      margin: EdgeInsets.only(
+                          top: 7.0, right: 7.0, left: 7.0, bottom: 15.0),
+                      child: InkWell(
+                          child: ClipRRect(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(35.0)),
+                              child: Stack(
+                                children: <Widget>[
+                                  Image.file(File(item),
+                                      fit: BoxFit.cover, width: 1000.0),
+                                ],
+                              )),
+                          onTap: () {
+                            final snackBar = SnackBar(
+                              backgroundColor: primeCOLOR,
+                              content: Text('Long press to remove image'),
+                              action: SnackBarAction(
+                                textColor: Colors.white,
+                                label: 'close',
+                                onPressed: () {},
+                              ),
+                            );
+                            ScaffoldMessenger.of(context)
+                                .showSnackBar(snackBar);
+                          },
+                          onLongPress: () {
+                            widget.onLongPress(item);
+                          }),
+                    ),
+                  ))
+              .toList();
+          break;
+        }
+      default:
+        {
+          imageSliders = [
+            Container(
+              child: Container(
+                margin: EdgeInsets.only(
+                    top: 7.0, right: 7.0, left: 7.0, bottom: 15.0),
+                child: ClipRRect(
+                    borderRadius: BorderRadius.all(Radius.circular(35.0)),
+                    child: Stack(
+                      children: <Widget>[
+                        Image.asset("images/404.png",
+                            fit: BoxFit.cover, width: 1000.0),
+                      ],
+                    )),
+              ),
+            )
+          ];
+          break;
+        }
+    }
     return Column(
       children: [
         CarouselSlider(
           items: imageSliders,
           options: CarouselOptions(
-              autoPlay: true,
+              autoPlay: widget.autoPlay,
               enlargeCenterPage: true,
               aspectRatio: 16 / 9,
               onPageChanged: (index, reason) {
@@ -30,8 +126,8 @@ class _CarouselWithIndicatorState extends State<CarouselWithIndicatorDemo> {
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: imgList.map((url) {
-            int index = imgList.indexOf(url);
+          children: widget.imageList.map((url) {
+            int index = widget.imageList.indexOf(url);
             return Container(
               width: 8.0,
               height: 8.0,
@@ -50,28 +146,3 @@ class _CarouselWithIndicatorState extends State<CarouselWithIndicatorDemo> {
     );
   }
 }
-
-final List<String> imgList = [
-  'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
-  'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
-  'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
-  'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
-  'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
-  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
-];
-
-final List<Widget> imageSliders = imgList
-    .map((item) => Container(
-          child: Container(
-            margin:
-                EdgeInsets.only(top: 7.0, right: 7.0, left: 7.0, bottom: 15.0),
-            child: ClipRRect(
-                borderRadius: BorderRadius.all(Radius.circular(35.0)),
-                child: Stack(
-                  children: <Widget>[
-                    Image.network(item, fit: BoxFit.cover, width: 1000.0),
-                  ],
-                )),
-          ),
-        ))
-    .toList();
