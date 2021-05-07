238632c6cf55faf1b970a623b92bf1e8c60ff283
akramAssi <55202260+akramAssi@users.noreply.github.com>
2021-05-08 01:22:53 +0300

    add list car color

--- lib/res/color.dart
+++ lib/res/color.dart
@@ -5,6 +5,28 @@ const Color annLable = Color(0xFFFFE13E);
 const Color BorderColor = Color(0x31707070);
 const Color fontColor = Color.fromARGB(255, 28, 33, 67);
 const Color redColor = Color.fromARGB(255, 198, 46, 46);
+const List<Color> carColor = [
+  Colors.white,
+  Colors.grey,
+  Colors.blueGrey,
+  Colors.black,
+  Colors.brown,
+  Colors.red,
+  Colors.pink,
+  Colors.purple,
+  Colors.purpleAccent,
+  Colors.blue,
+  Colors.lightBlue,
+  Colors.lightBlueAccent,
+  Colors.blueAccent,
+  Colors.green,
+  Colors.lightGreen,
+  Colors.greenAccent,
+  Colors.orange,
+  Colors.orangeAccent,
+  Colors.yellow,
+  Colors.yellowAccent,
+];
 MaterialColor primeCOLOR = MaterialColor(0xFF212F91, <int, Color>{
   50: Color.fromRGBO(136, 14, 79, .1),
   100: Color.fromRGBO(136, 14, 79, .2),
