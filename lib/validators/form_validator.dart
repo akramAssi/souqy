bc9b5d84ef99a4ade05ded901d223d7dcfd3e0db
akrma Assi <akram.20002010@gmail.com>
2021-05-08 11:39:47 +0300

    create FormValidator

---
+++ lib/validators/form_validator.dart
@@ -0,0 +1,6 @@
+import 'package:souqy/res/car.dart';
+
+class FormValidator {
+  static bool isEmpty(String value) => value.isEmpty;
+  static bool isBrand(String value) => brands.contains(value);
+}
