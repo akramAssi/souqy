8184ccdf4dbd0b8b5f3b4504002d07b5652d1744
akrma Assi <akram.20002010@gmail.com>
2021-05-08 11:39:23 +0300

    create  file contain error message

---
+++ lib/validators/error_text.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:souqy/res/color.dart';
+
+const String brandErrorText = "Please enter a valid make";
+
+const TextStyle errorStyle =
+    TextStyle(fontSize: 10, height: 1, color: redColor);
