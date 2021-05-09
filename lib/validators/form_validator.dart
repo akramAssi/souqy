98da1505296d8282cb64c048d1a8d61ba2c221dd
akramAssi <55202260+akramAssi@users.noreply.github.com>
2021-05-09 11:36:19 +0300

    add method isBrandValid

--- lib/validators/form_validator.dart
+++ lib/validators/form_validator.dart
@@ -3,4 +3,5 @@ import 'package:souqy/res/car.dart';
 class FormValidator {
   static bool isEmpty(String value) => value.isEmpty;
   static bool isBrand(String value) => brands.contains(value);
+  static bool isBrandValid(String value) => !(isEmpty(value) && isBrand(value));
 }
