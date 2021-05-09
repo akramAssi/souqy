21d07b6bb1aef88a84c40ae858be3f9b1b402d72
akramAssi <55202260+akramAssi@users.noreply.github.com>
2021-05-09 12:50:51 +0300

    create class have all string in app

---
+++ lib/res/string.dart
@@ -0,0 +1,10 @@
+class Strings {
+  static const String appName = "SOUQY";
+  static const String e_mail = "E-mail";
+  static const String password = "Password";
+  static const String login = "login";
+  static const String orLoginWith = "or Login with";
+  static const String createaNewAccount = "create a new account?";
+  static const String alreadyHaveAccount = "already have account?";
+  static const String visitor = "visitor";
+}
