ac045ffa8d0ef464e2004e5c497011558b6ae61f
akramAssi <55202260+akramAssi@users.noreply.github.com>
2021-05-09 11:39:29 +0300

    create mixin SouqyTextfieldStyle have all style for all text field in app

---
+++ lib/res/style.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+import 'color.dart';
+
+mixin SouqyTextfieldStyle {
+  BorderRadius radius = BorderRadius.circular(20);
+  //return to last TextField
+  TextSelection returnToLast(controller) {
+    return TextSelection.fromPosition(
+        TextPosition(offset: controller.text.length));
+  }
+
+  InputDecoration get searchDecoration => InputDecoration(
+        prefixIcon: Icon(
+          Icons.search,
+          color: Colors.grey,
+        ),
+        //icon search
+        labelText: "Make",
+        labelStyle: TextStyle(fontSize: 18, color: primeCOLOR),
+        //prepare lable
+        filled: true,
+        fillColor: backgroundColor,
+        //set backgtound
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: borderColor),
+          borderRadius: radius,
+        ),
+        //set border in normal mode
+        focusedBorder: OutlineInputBorder(
+          borderRadius: radius,
+          borderSide: BorderSide(color: primeCOLOR),
+        ),
+        //set border in foucus mode
+      );
+  OutlineInputBorder get souqyFocusBorder => OutlineInputBorder(
+        borderSide: BorderSide(color: primeCOLOR),
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(20),
+          topLeft: Radius.zero,
+          bottomLeft: Radius.circular(20),
+          bottomRight: Radius.circular(20),
+        ),
+      );
+  OutlineInputBorder get souqyEnableBorder => OutlineInputBorder(
+        borderSide: BorderSide(color: borderTextfieldColor),
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(20),
+          topLeft: Radius.zero,
+          bottomLeft: Radius.circular(20),
+          bottomRight: Radius.circular(20),
+        ),
+      );
+}
