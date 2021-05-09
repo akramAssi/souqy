5fe53dd9544cf7a96b15d1d08f70334c5e1de618
akramAssi <55202260+akramAssi@users.noreply.github.com>
2021-05-09 12:03:46 +0300

    change  name file

---
+++ lib/add_page/souqy_search_filed.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import '../res/color.dart';
+import '../res/style.dart';
+
+class SouqySearchFiled extends StatefulWidget with SouqyTextfieldStyle {
+  final void Function(String) onChange;
+  final FocusNode focusNode;
+  final controller;
+  SouqySearchFiled({
+    key,
+    this.onChange,
+    this.controller,
+    @required this.focusNode,
+  }) : super(key: key);
+
+  @override
+  _SouqySearchFiledState createState() => _SouqySearchFiledState();
+}
+
+class _SouqySearchFiledState extends State<SouqySearchFiled> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 80,
+      padding: EdgeInsets.symmetric(horizontal: 17, vertical: 16),
+      child: TextField(
+        controller: widget.controller,
+        focusNode: widget.focusNode,
+        textInputAction: TextInputAction.next,
+        cursorColor: fontColor,
+        style: TextStyle(fontSize: 17, color: fontColor),
+        decoration: widget.searchDecoration,
+        onChanged: (value) {
+          widget.onChange(value);
+        },
+      ),
+    );
+  }
+}
