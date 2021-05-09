1121e966dabca9539e749e3c54a58a24e61bcc4c
akramAssi <55202260+akramAssi@users.noreply.github.com>
2021-05-09 13:15:11 +0300

    update

--- lib/res/string.dart
+++ lib/res/string.dart
@@ -4,7 +4,8 @@ class Strings {
   static const String password = "Password";
   static const String login = "login";
   static const String orLoginWith = "or Login with";
-  static const String createaNewAccount = "create a new account?";
+  static const String createNewAccount = "create a new account?";
+  static const String createAccount = "create a account?";
   static const String alreadyHaveAccount = "already have account?";
   static const String visitor = "visitor";
   static const String name = "Name";
