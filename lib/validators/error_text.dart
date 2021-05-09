cc32e3a91342c30239ecf7723a5fa02e2a738784
akramAssi <55202260+akramAssi@users.noreply.github.com>
2021-05-09 11:36:52 +0300

    change name color for errorStyle

--- lib/validators/error_text.dart
+++ lib/validators/error_text.dart
@@ -4,4 +4,4 @@ import 'package:souqy/res/color.dart';
 const String brandErrorText = "Please enter a valid make";
 
 const TextStyle errorStyle =
-    TextStyle(fontSize: 10, height: 1, color: redColor);
+    TextStyle(fontSize: 10, height: 1, color: alertColor);
