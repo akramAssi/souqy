4f23d5c65331ff79f8782fed29606a629d84df6e
akramAssi <55202260+akramAssi@users.noreply.github.com>
2021-05-10 14:45:16 +0300

    delete unnecessary file

--- lib/validators/error_text.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:souqy/res/color.dart';
-
-const String brandErrorText = "Please enter a valid make";
-
-const TextStyle errorStyle =
-    TextStyle(fontSize: 10, height: 1, color: alertColor);
--- lib/validators/form_validator.dart
+++
@@ -1,7 +0,0 @@
-import 'package:souqy/res/car.dart';
-
-class FormValidator {
-  static bool isEmpty(String value) => value.isEmpty;
-  static bool isBrand(String value) => brands.contains(value);
-  static bool isBrandValid(String value) => !(isEmpty(value) && isBrand(value));
-}
