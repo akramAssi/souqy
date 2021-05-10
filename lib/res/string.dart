1842926bc403bd0c9b8fe978afef72b467c2ccdf
akramAssi <55202260+akramAssi@users.noreply.github.com>
2021-05-10 14:41:44 +0300

    update

--- lib/res/string.dart
+++ lib/res/string.dart
@@ -2,7 +2,7 @@ class Strings {
   static const String appName = "SOUQY";
   static const String e_mail = "E-mail";
   static const String password = "Password";
-  static const String login = "login";
+  static const String login = "Login";
   static const String orLoginWith = "or Login with";
   static const String createNewAccount = "create a new account?";
   static const String createAccount = "create a account?";
@@ -10,4 +10,9 @@ class Strings {
   static const String visitor = "visitor";
   static const String name = "Name";
   static const String phone = "Phone";
+  static const String e_mailInValid = "$e_mail is required";
+  static const String passwordInValidRequired = "$password is required";
+  static const String passwordInValidLength =
+      "should be 8 characters or higher";
+  static String success(String value) => "$value success";
 }
