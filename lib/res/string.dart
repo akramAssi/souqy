0e05427aa8bac84297cbf76f983a6b4406e2417e
akramAssi <55202260+akramAssi@users.noreply.github.com>
2021-05-11 13:12:21 +0300

    add new strings

--- lib/res/string.dart
+++ lib/res/string.dart
@@ -5,14 +5,31 @@ class Strings {
   static const String login = "Login";
   static const String orLoginWith = "or Login with";
   static const String createNewAccount = "create a new account?";
-  static const String createAccount = "create a account?";
+  static const String createAccount = "create a account";
   static const String alreadyHaveAccount = "already have account?";
   static const String visitor = "visitor";
   static const String name = "Name";
+  static const String city = "City";
+  static const String area = "Area";
   static const String phone = "Phone";
-  static const String e_mailInValid = "$e_mail is required";
+  static const String nameRequired = "$name is required";
+  static const String emailInValid = "$e_mail is invalid";
+  static const String emailRequired = "$e_mail is required";
+  static const String phoneInValidRequired = "$phone is required";
+  static const String phoneInValidLength = "should be 10 number";
+  static const String phoneInValidPrefix = "should be start 05";
+  static const String phoneInValidNotString = "should be diget number";
+  static const String acceptExceptionDialog = "ok";
   static const String passwordInValidRequired = "$password is required";
   static const String passwordInValidLength =
       "should be 8 characters or higher";
+
+  static const String failedTitle = "failed";
+  static const String userFetchFailed = "user fetch failed";
+  static const String signInAnonmymous = "$createAccount Anonmymous failed";
+  static const String signOut = "signOut";
+  static const String signFailed = "$signOut  failed";
+  static const String upLoad = "upload";
+  static const String upLoadFailed = "$upLoad  failed";
   static String success(String value) => "$value success";
 }
