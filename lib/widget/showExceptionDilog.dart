1d2c1ebd25e1a5e81455f6482302a35bbc8b4b33
akramAssi <55202260+akramAssi@users.noreply.github.com>
2021-05-11 13:07:19 +0300

    fix message dialog

--- lib/widget/showExceptionDilog.dart
+++ lib/widget/showExceptionDilog.dart
@@ -2,20 +2,21 @@ import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:souqy/res/string.dart';
 
 Future<void> showExceptionDialog(BuildContext context,
-    {Text title, String content}) async {
+    {String title, String content}) async {
   return showDialog<void>(
     context: context,
     barrierDismissible: false, // user must tap button!
     builder: (context) {
       if (Platform.isIOS) {
         return CupertinoAlertDialog(
-          title: title,
+          title: Text(title),
           content: Text(content),
           actions: <Widget>[
             CupertinoDialogAction(
-              child: Text('Approve'),
+              child: Text(Strings.acceptExceptionDialog),
               onPressed: () {
                 Navigator.of(context).pop();
               },
@@ -24,11 +25,11 @@ Future<void> showExceptionDialog(BuildContext context,
         );
       }
       return AlertDialog(
-        title: title,
+        title: Text(title),
         content: Text(content),
         actions: <Widget>[
           TextButton(
-            child: Text('Approve'),
+            child: Text(Strings.acceptExceptionDialog),
             onPressed: () {
               Navigator.of(context).pop();
             },
