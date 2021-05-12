2e766d554c0cc0c3e83fdd27e6b66305f234db8c
akrma Assi <akram.20002010@gmail.com>
2021-05-12 10:06:59 +0300

    remove unnecessary code

--- lib/widget/showExceptionDilog.dart
+++ lib/widget/showExceptionDilog.dart
@@ -39,29 +39,3 @@ Future<void> showExceptionDialog(BuildContext context,
     },
   );
 }
-
-// Future<void> showMyDialog(BuildContext context1) async {
-//   return
-// }
-
-Widget crr(BuildContext context) {
-  return AlertDialog(
-    title: Text('AlertDialog Title'),
-    content: SingleChildScrollView(
-      child: ListBody(
-        children: <Widget>[
-          Text('This is a demo alert dialog.'),
-          Text('Would you like to approve of this message?'),
-        ],
-      ),
-    ),
-    actions: <Widget>[
-      TextButton(
-        child: Text('Approve'),
-        onPressed: () {
-          Navigator.of(context).pop();
-        },
-      ),
-    ],
-  );
-}
