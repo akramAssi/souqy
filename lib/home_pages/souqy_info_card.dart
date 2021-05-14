9f3d0877f3c04bc8ee0734ba7f048bbc3a1c5d0c
akramAssi <55202260+akramAssi@users.noreply.github.com>
2021-05-14 11:21:15 +0300

    remove Directionality  and fix ltr

--- lib/home_pages/souqy_info_card.dart
+++ lib/home_pages/souqy_info_card.dart
@@ -11,71 +11,68 @@ class SouqyInfoCard extends StatelessWidget {
       : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Directionality(
-      textDirection: TextDirection.rtl,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(right: 8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  make,
-                  style: TextStyle(
-                    fontSize: 25,
-                    color: fontColor,
-                  ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                make,
+                style: TextStyle(
+                  fontSize: 25,
+                  color: fontColor,
                 ),
-                Text(
-                  model,
-                  style: TextStyle(
-                    fontSize: 23,
-                    color: fontColor,
-                  ),
+              ),
+              Text(
+                model,
+                style: TextStyle(
+                  fontSize: 23,
+                  color: fontColor,
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-          SizedBox(
-            height: 12,
+        ),
+        SizedBox(
+          height: 12,
+        ),
+        Container(
+          alignment: Alignment(-5, 0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+                topRight: Radius.circular(25),
+                bottomRight: Radius.circular(25)),
+            color: alertColor,
           ),
-          Container(
-            alignment: Alignment(-5, 0),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(25),
-                  bottomLeft: Radius.circular(25)),
-              color: alertColor,
-            ),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 12, vertical: 9),
-                  child: Image.asset(
-                    'images/shekel.png',
-                    width: 15,
-                  ),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12, vertical: 9),
+                child: Image.asset(
+                  'images/shekel.png',
+                  width: 15,
                 ),
-                Text(
-                  '$price',
-                  style: TextStyle(
-                    fontSize: size.width / 23,
-                    color: Colors.white,
-                  ),
+              ),
+              Text(
+                '$price',
+                style: TextStyle(
+                  fontSize: size.width / 23,
+                  color: backgroundColor,
                 ),
-                SizedBox(
-                  width: 22,
-                )
-              ],
-            ),
+              ),
+              SizedBox(
+                width: 22,
+              )
+            ],
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
