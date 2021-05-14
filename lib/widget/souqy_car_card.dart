1e881909bea7ec9a04fa58b38548f8acfb3159d8
akramAssi <55202260+akramAssi@users.noreply.github.com>
2021-05-14 11:14:53 +0300

    change source for data card

--- lib/widget/souqy_car_card.dart
+++ lib/widget/souqy_car_card.dart
@@ -3,6 +3,7 @@ import 'package:souqy/home_pages/souqy_Info_Circle_Card.dart';
 import 'package:souqy/home_pages/souqy_available_label.dart';
 import 'package:souqy/home_pages/souqy_info_card.dart';
 import 'package:souqy/home_pages/souqy_thumbnail_card.dart';
+import 'package:souqy/model/ads.dart';
 import 'package:souqy/moreInfoPage/moreInfoPage.dart';
 
 int index = 0;
@@ -13,15 +14,19 @@ class SouqyCarCard extends StatelessWidget {
   SouqyCarCard({
     Key key,
     @required this.size,
+    @required this.carAdsInfo,
     // this.index,
   }) : super(key: key);
 
   final Size size;
+  final Ads carAdsInfo;
 
   void _openMoreInfo(BuildContext context) {
     Navigator.of(context).push(
       MaterialPageRoute<void>(
-        builder: (context) => MoreInfoPage(),
+        builder: (context) => MoreInfoPage(
+          carAdsInfo: carAdsInfo,
+        ),
         fullscreenDialog: true,
       ),
     );
@@ -49,46 +54,48 @@ class SouqyCarCard extends StatelessWidget {
           mainAxisSize: MainAxisSize.min,
           children: [
             Stack(children: <Widget>[
-              SouqyThumbnailCard(path: 'images/kia-op.jpg'),
+              // SouqyThumbnailCard(path: 'images/kia-op.jpg'),
+              SouqyThumbnailCard(path: carAdsInfo.card.thumbnailUrl),
               Positioned(
-                right: size.width / 80,
+                left: size.width / 80,
                 top: 25.0,
                 child: SouqyAvailabellabel(
                   size: size,
-                  availabel: true,
+                  availabel: carAdsInfo.card.available,
                   isCard: true,
                 ),
               ),
             ]),
             Padding(
-              padding: const EdgeInsets.only(top: 8.0, bottom: 8.0, left: 8.0),
+              padding: const EdgeInsets.only(top: 8.0, bottom: 8.0, right: 8.0),
               child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   SouqyInfoCard(
-                    make: "مرسيدس",
-                    model: "c300",
-                    price: 115000,
+                    make: carAdsInfo.card.make,
+                    model: carAdsInfo.card.model,
+                    price: carAdsInfo.card.price,
                     size: size,
                   ),
                   Spacer(),
                   Column(
                     children: [
                       SouqyInfoCircleCard(
-                        value: "${2012}",
+                        value: "${carAdsInfo.card.year}",
                         path: "images/year.png",
                       ),
                       SizedBox(
                         height: 5,
                       ),
                       SouqyInfoCircleCard(
-                        value: "private",
+                        value: "${carAdsInfo.card.origin}",
                         path: "images/mini_car.png",
                       ),
                       SizedBox(
                         height: 5,
                       ),
                       SouqyInfoCircleCard(
-                        value: "petrol",
+                        value: "${carAdsInfo.card.fuel}",
                         path: "images/gas_station.png",
                       ),
                     ],
