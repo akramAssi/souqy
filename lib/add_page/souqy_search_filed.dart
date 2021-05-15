420da731dda326085bca9b287e02acd716805a0b
akrma Assi <akram.20002010@gmail.com>
2021-05-15 12:42:14 +0300

    add Focus

--- lib/add_page/souqy_search_filed.dart
+++ lib/add_page/souqy_search_filed.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:souqy/res/car.dart';
 import 'package:souqy/res/string.dart';
 import 'package:wc_form_validators/wc_form_validators.dart';
 
@@ -9,11 +10,13 @@ class SouqySearchFiled extends StatefulWidget with SouqyFormFieldStyle {
   final void Function(String) onChange;
   final FocusNode focusNode;
   final controller;
+  final VoidCallback lossFocus;
   SouqySearchFiled({
     key,
     this.onChange,
     this.controller,
     @required this.focusNode,
+    this.lossFocus,
   }) : super(key: key);
 
   @override
@@ -27,18 +30,30 @@ class _SouqySearchFiledState extends State<SouqySearchFiled> {
       // height: 90,
       constraints: BoxConstraints(minHeight: 70),
       padding: EdgeInsets.symmetric(horizontal: 17, vertical: 16),
-      child: TextFormField(
-        controller: widget.controller,
-        focusNode: widget.focusNode,
-        textInputAction: TextInputAction.next,
-        cursorColor: fontColor,
-        style: TextStyle(fontSize: 17, color: fontColor),
-        decoration: widget.searchDecoration,
-        autovalidateMode: AutovalidateMode.onUserInteraction,
-        validator: Validators.required(Strings.requiredField(Strings.make)),
-        onChanged: (value) {
-          widget.onChange(value);
+      child: Focus(
+        onFocusChange: (focus) {
+          if (focus == false) {
+            widget.lossFocus();
+          }
         },
+        child: TextFormField(
+          controller: widget.controller,
+          focusNode: widget.focusNode,
+          textInputAction: TextInputAction.next,
+          cursorColor: fontColor,
+          style: TextStyle(fontSize: 17, color: fontColor),
+          decoration: widget.searchDecoration,
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+          validator: (String value) {
+            if (Validators.required(Strings.requiredField(Strings.make)) ==
+                null) return Strings.requiredField(Strings.make);
+            if (!brands.contains(value)) return "must be a brand";
+            return null;
+          },
+          onChanged: (value) {
+            widget.onChange(value);
+          },
+        ),
       ),
     );
   }
