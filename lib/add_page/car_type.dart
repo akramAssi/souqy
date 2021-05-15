7e10e04da875153e65588e9b5ab01074f7f0ddd5
akrma Assi <akram.20002010@gmail.com>
2021-05-15 12:43:08 +0300

    add padding for icon

--- lib/add_page/car_type.dart
+++ lib/add_page/car_type.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:souqy/res/car.dart';
 import 'package:souqy/res/color.dart';
@@ -41,8 +42,8 @@ class _CarType extends State<CarType> {
     var size = MediaQuery.of(context).size;
     print(widget.loc);
     return Container(
-      margin: EdgeInsets.only(top: 5),
-      padding: EdgeInsets.symmetric(vertical: 10),
+      // margin: EdgeInsets.only(top: 5),
+      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
       height: widget.setSelected != 0 ? 180 : 80,
       child: GridView.builder(
           shrinkWrap: true,
@@ -54,29 +55,32 @@ class _CarType extends State<CarType> {
                     : size.width / 6,
             mainAxisSpacing: 15,
             crossAxisSpacing: 10,
-            childAspectRatio: 3.8,
+            childAspectRatio: 3,
           ),
           itemCount: widget.onchangeList.length,
           itemBuilder: (BuildContext ctx, index) {
             return ChoiceChip(
-              avatar: widget.setSelected == 0
-                  ? Image.asset(
-                      'images/carType/' + carTypeList[widget.loc] + '.png',
-                      width: 30,
-                      color: fontColor,
-                    )
-                  : Image.asset(
-                      'images/carType/' + carTypeList[index] + '.png',
-                      width: 30,
-                      color: fontColor,
-                    ),
+              avatar: Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: widget.setSelected == 0
+                    ? Image.asset(
+                        'images/carType/' + carTypeList[widget.loc] + '.png',
+                        width: 30,
+                        color: fontColor,
+                      )
+                    : Image.asset(
+                        'images/carType/' + carTypeList[index] + '.png',
+                        width: 30,
+                        color: fontColor,
+                      ),
+              ),
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10),
                   side: BorderSide(color: borderColor, width: 1.0)),
               label: Container(
                 alignment: Alignment.center,
                 width: 60,
-                child: Text(widget.onchangeList[index]),
+                child: AutoSizeText(widget.onchangeList[index]),
               ),
               selected: widget.setSelected == index,
               selectedColor: Colors.grey,
