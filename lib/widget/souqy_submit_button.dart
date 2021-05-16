bbc58b3fe11d94647aeceef4e36acaff62242459
akramAssi <55202260+akramAssi@users.noreply.github.com>
2021-05-16 12:35:06 +0300

    add icon to button

--- lib/widget/souqy_submit_button.dart
+++ lib/widget/souqy_submit_button.dart
@@ -11,37 +11,63 @@ class SouqySubmitBotton extends StatelessWidget {
   final String label;
   final double height;
   final double fontSize;
+  final icon;
   const SouqySubmitBotton(
       {Key key,
       @required this.onPress,
       @required this.label,
       this.height = 40,
-      this.fontSize = 17})
+      this.fontSize = 17,
+      this.icon})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: height,
-      child: ElevatedButton(
+    Widget content;
+    if (icon == null) {
+      content = ElevatedButton(
         onPressed: onPress,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 9.0, horizontal: 61.0),
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: fontSize,
-              color: backgroundColor,
-            ),
+        child: Text(
+          label,
+          style: TextStyle(
+            fontSize: fontSize,
+            color: backgroundColor,
           ),
         ),
         style: ElevatedButton.styleFrom(
+          padding: const EdgeInsets.symmetric(vertical: 9.0, horizontal: 61.0),
           onPrimary: backgroundColor,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(11),
           ),
         ),
-      ),
+      );
+    } else {
+      content = ElevatedButton.icon(
+        onPressed: onPress,
+        icon: Image.asset(
+          icon,
+          color: backgroundColor,
+        ),
+        label: Text(
+          label,
+          style: TextStyle(
+            fontSize: fontSize,
+            color: backgroundColor,
+          ),
+        ),
+        style: ElevatedButton.styleFrom(
+          padding: const EdgeInsets.symmetric(vertical: 9.0, horizontal: 61.0),
+          onPrimary: backgroundColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(11),
+          ),
+        ),
+      );
+    }
+    return SizedBox(
+      height: height,
+      child: content,
     );
   }
 }
