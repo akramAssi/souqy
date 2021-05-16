a265602f72fe76194a515e1f1dd15e1169add09b
akramAssi <55202260+akramAssi@users.noreply.github.com>
2021-05-16 16:32:12 +0300

    add SouqyInstallmentsLable

--- lib/home_pages/souqy_info_card.dart
+++ lib/home_pages/souqy_info_card.dart
@@ -1,14 +1,23 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:souqy/home_pages/souqy_available_label.dart';
 import 'package:souqy/res/color.dart';
+import 'package:souqy/res/string.dart';
 
 class SouqyInfoCard extends StatelessWidget {
   final String make;
   final String model;
+  final String paymentMethod;
   final int price;
   final Size size;
 
-  SouqyInfoCard({Key key, this.make, this.model, this.price, this.size})
+  SouqyInfoCard(
+      {Key key,
+      this.make,
+      this.model,
+      this.price,
+      this.size,
+      this.paymentMethod})
       : super(key: key);
   @override
   Widget build(BuildContext context) {
@@ -79,7 +88,51 @@ class SouqyInfoCard extends StatelessWidget {
             ],
           ),
         ),
+        SizedBox(
+          height: 4,
+        ),
+        (paymentMethod != null && paymentMethod.contains(Strings.installment))
+            ? SouqyInstallmentsLable(
+                size: size,
+              )
+            : SizedBox(),
       ],
+    );
+  }
+}
+
+class SouqyInstallmentsLable extends StatelessWidget {
+  SouqyInstallmentsLable({
+    Key key,
+    @required this.size,
+  }) : super(key: key);
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    // BorderRadiusGeometry radius =
+
+    return Container(
+      // width: 70,
+      // height: 23,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(
+            topRight: Radius.circular(10), bottomRight: Radius.circular(10)),
+        color: borderColor,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(
+          Strings.installment,
+          style: TextStyle(
+            fontSize: 10,
+            color: primeCOLOR,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ),
     );
   }
 }
