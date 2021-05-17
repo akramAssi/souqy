5205fb9aa93a73658af254659573bb089b5bc90a
akramAssi <55202260+akramAssi@users.noreply.github.com>
2021-05-17 20:28:41 +0300

    handle change in userModel

--- lib/view_controller/user_controller.dart
+++ lib/view_controller/user_controller.dart
@@ -4,13 +4,11 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:souqy/model/user_model.dart';
-import 'package:souqy/res/string.dart';
 import 'package:souqy/service/Auth.dart';
 import 'package:souqy/service/database_repo.dart';
 import 'package:souqy/service/locator.dart';
 import 'package:souqy/service/storage_repo.dart';
 import 'package:souqy/service/firebase_auth_exceptions_handling.dart';
-import 'package:souqy/widget/showExceptionDilog.dart';
 
 class UserController {
   UserModel _currentUser;
@@ -55,7 +53,14 @@ class UserController {
   Future<void> signInWithGoogle(BuildContext context) async {
     try {
       User user = await _authRepo.mysignInWithGoogle(context);
+      // UserModel tempUser = UserModel.user(user);
       _currentUser = UserModel.user(user);
+      await readAddres();
+      if (_currentUser.displayName == null ||
+          _currentUser.displayName.isEmpty) {
+        _currentUser.displayName = user.displayName;
+        storeSelf();
+      }
       await getDownloadUrl();
     } catch (e) {
       print(e.toString());
@@ -66,6 +71,13 @@ class UserController {
     try {
       User user = await _authRepo.signInWithFacebook();
       _currentUser = UserModel.user(user);
+      await readAddres();
+      if (_currentUser.displayName == null ||
+          _currentUser.displayName.isEmpty) {
+        _currentUser.displayName = user.displayName;
+        print("eeeokko:${_currentUser.displayName}");
+        storeSelf();
+      }
       getDownloadUrl();
     } catch (e) {
       print(e.toString());
@@ -79,6 +91,13 @@ class UserController {
       if (user != null) {
         _status = AuthResultStatus.successful;
         _currentUser = UserModel.user(user);
+        await readAddres();
+        if (_currentUser.displayName == null ||
+            _currentUser.displayName.isEmpty) {
+          _currentUser.displayName = user.displayName;
+          print("eeeokko:${_currentUser.displayName}");
+          storeSelf();
+        }
       } else {
         _status = AuthResultStatus.undefined;
       }
@@ -97,12 +116,12 @@ class UserController {
   }) async {
     try {
       User user =
-          await _authRepo.creatUserWithEmailAndPassword(name, email, password);
+          await _authRepo.creatUserWithEmailAndPassword(email, password);
       // user = await _authRepo.updateDisplayName(name);
       // _currentUser = UserModel.user(user);
 
       // return false;
-      storeAddress(phone: phone);
+      storeAddress(name: name, phone: phone, email: email);
 
       if (user != null) {
         _status = AuthResultStatus.successful;
@@ -141,10 +160,30 @@ class UserController {
   //database
 
   Future<void> storeAddress(
-      {@required String phone, String city, String area}) async {
-    _currentUser.phone = phone ?? "";
-    _currentUser.city = city ?? "";
-    _currentUser.area = area ?? "";
+      {@required String name,
+      @required String email,
+      @required String phone,
+      String city,
+      String area}) async {
+    UserModel temp = UserModel(
+      _currentUser.uid,
+      avatarUrl: _currentUser.avatarUrl,
+      displayName: name,
+      email: email,
+      phone: phone,
+      city: city,
+      area: area,
+    );
+    await _databaseRepo.storeUserInfo(temp).then((value) {
+      _currentUser = temp;
+      // _currentUser.email = email ?? "";
+      // _currentUser.phone = phone ?? "";
+      // _currentUser.city = city ?? "";
+      // _currentUser.area = area ?? "";
+    });
+  }
+
+  Future<void> storeSelf() async {
     await _databaseRepo.storeUserInfo(_currentUser);
   }
 
