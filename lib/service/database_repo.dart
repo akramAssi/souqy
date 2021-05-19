5c63098d17e4f6519e63f7bfb7c5d7721af1867d
akramAssi <55202260+akramAssi@users.noreply.github.com>
2021-05-19 12:09:27 +0300

    add readAds and test userInNablus

--- lib/service/database_repo.dart
+++ lib/service/database_repo.dart
@@ -1,14 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:souqy/model/ads.dart';
 import 'package:souqy/model/user_model.dart';
 import 'package:souqy/widget/showExceptionDilog.dart';
 
 abstract class Database {
   Future<void> storeUserInfo(UserModel user);
-  void readUserInfo(UserModel user);
+  void readUserInfo(UserModel user, String userId);
 }
 
-class FirestoreDatabase implements Database {
+class FirestoreDatabase {
   // FirestoreDatabase({@required this.uid});
   // final String uid;
   final _firestore = FirebaseFirestore.instance;
@@ -16,30 +17,32 @@ class FirestoreDatabase implements Database {
   Future<void> storeUserInfo(UserModel user) async {
     final String path = "/user/${user.uid}";
     final firestoreRef = _firestore.doc(path);
-    await firestoreRef.set(user.address);
+    await firestoreRef.set(user.toJson());
   }
 
-  Future<void> readUserInfo(UserModel user) async {
+  Future<Map<String, dynamic>> readUserInfo(String userId) async {
     // try {
     // throw IntegerDivisionByZeroException;
     // print("dsadsad");
-    final String path = "/user/${user.uid}";
+    final String path = "/user/$userId";
     final firestoreRef = _firestore.doc(path);
+    Map<String, dynamic> data;
     await firestoreRef.get().then((document) {
-      final data = document.data();
+      data = document.data();
       if (data != null) {
-        print(data);
-        user.setAddress(
-            phone: data["phone"] ?? "",
-            city: data["city"] ?? "",
-            area: data["area"] ?? "");
-
-        print(
-            "firebase : ${data["phone"]} ,  ${data["city"]} , ${data["area"]}");
-        print("obj ${user.phone} ,  ${user.city} , ${user.area}");
+        return data;
+        // user.setData(
+        //     name: data["name"],
+        //     email: data["email"],
+        //     phone: data["phone"] ?? "",
+        //     city: data["city"] ?? "",
+        //     area: data["area"] ?? "");
+        // print(
+        //     "firebase : ${data["phone"]} ,  ${data["city"]} , ${data["area"]}");
+        // print("obj ${user.phone} ,  ${user.city} , ${user.area}");
       }
     });
-
+    return data;
     //   print(path);
     // } catch (e) {
     //   print("dsad");
@@ -56,5 +59,59 @@ class FirestoreDatabase implements Database {
 
     //   },
     // );
+  }
+
+  Future<void> userInNablus() async {
+    // try {
+    // throw IntegerDivisionByZeroException;
+    // print("dsadsad");
+    // final String path = "/user/${user.uid}";
+    final firestoreRef =
+        _firestore.collection("user").where("city", isEqualTo: "Qalqilya");
+    // final snapshot = firestoreRef.snapshots();
+    // snapshot.listen((event) {
+    //   event.docs.forEach((element) {
+    //     print(element.data());
+    //   });
+    // });
+    print("hi  hi  hi");
+    await firestoreRef.get().then((value) {
+      value.docs.forEach((element) {
+        print("-------------------------------------");
+        print(element.id);
+        print(element.data());
+      });
+    });
+  }
+
+  Future<void> createAds(data) async {
+    // final String path = "/user/${user.uid}";
+    var temp = {
+      "cardInfo": {"id": "23232323"}
+    };
+    await _firestore.collection("ads").add(data);
+  }
+
+  Stream<QuerySnapshot<Map<String, dynamic>>> readAds() {
+    final firestoreRef = _firestore.collection("ads");
+    final snapshot = firestoreRef.snapshots();
+    return snapshot;
+    // snapshot.listen((event) {
+    //   event.docs.forEach((element) {
+    //     print(element.data());
+    //   });
+    // });
+    /////////////////
+    // List<Ads> x = [];
+    // print("hi  hi  hi");
+    // await firestoreRef.get().then((value) {
+    //   value.docs.forEach((element) {
+    //     print("-------------------------------------");
+    //     print(element.id);
+    //     print(element.data());
+    //     x.add(Ads.fromJson(element.data()));
+    //   });
+    // });
+    // return x;
   }
 }
