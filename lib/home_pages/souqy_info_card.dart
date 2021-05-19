4aaa9db25119eb8c648b9cac7060c79956e032a0
akramAssi <55202260+akramAssi@users.noreply.github.com>
2021-05-19 12:07:26 +0300

    fixed layout

--- lib/home_pages/souqy_info_card.dart
+++ lib/home_pages/souqy_info_card.dart
@@ -29,25 +29,33 @@ class SouqyInfoCard extends StatelessWidget {
         Padding(
           padding: const EdgeInsets.only(left: 8.0),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              AutoSizeText(
-                make,
-                maxLines: 1,
-                minFontSize: 5,
-                maxFontSize: 40,
-                style: TextStyle(
-                  fontSize: size.width / 22,
-                  color: fontColor,
+              Container(
+                alignment: Alignment.centerLeft,
+                height: size.width / 17,
+                child: AutoSizeText(
+                  make,
+                  maxLines: 1,
+                  minFontSize: 5,
+                  maxFontSize: 40,
+                  style: TextStyle(
+                    fontSize: size.width / 22,
+                    color: fontColor,
+                  ),
                 ),
               ),
-              AutoSizeText(
-                model,
-                maxLines: 1,
-                minFontSize: 5,
-                style: TextStyle(
-                  fontSize: size.width / 23,
-                  color: fontColor,
+              Container(
+                alignment: Alignment.centerLeft,
+                height: size.width / 18,
+                child: AutoSizeText(
+                  model,
+                  maxLines: 1,
+                  minFontSize: 5,
+                  style: TextStyle(
+                    fontSize: size.width / 23,
+                    color: fontColor,
+                  ),
                 ),
               ),
             ],
@@ -57,35 +65,24 @@ class SouqyInfoCard extends StatelessWidget {
           height: 12,
         ),
         Container(
-          alignment: Alignment(-5, 0),
+          constraints: const BoxConstraints(minWidth: double.infinity),
+          // alignment: Alignment(-5, 0),
+          padding: EdgeInsets.only(right: 10, top: 7, bottom: 7, left: 10),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.only(
                 topRight: Radius.circular(25),
                 bottomRight: Radius.circular(25)),
             color: alertColor,
           ),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 12, vertical: 9),
-                child: Image.asset(
-                  'images/shekel.png',
-                  width: size.width / 24,
-                ),
-              ),
-              Text(
-                '$price',
-                style: TextStyle(
-                  fontSize: size.width / 24,
-                  color: backgroundColor,
-                ),
-              ),
-              SizedBox(
-                width: size.width / 25,
-              )
-            ],
+          child: Text(
+            '₪ $price',
+            style: TextStyle(
+              fontSize:
+                  MediaQuery.of(context).orientation == Orientation.portrait
+                      ? size.width / 24
+                      : size.width / 30,
+              color: backgroundColor,
+            ),
           ),
         ),
         SizedBox(
@@ -113,26 +110,31 @@ class SouqyInstallmentsLable extends StatelessWidget {
   Widget build(BuildContext context) {
     // BorderRadiusGeometry radius =
 
-    return Container(
-      // width: 70,
-      // height: 23,
-      alignment: Alignment.center,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.only(
-            topRight: Radius.circular(10), bottomRight: Radius.circular(10)),
-        color: borderColor,
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Text(
-          Strings.installment,
-          style: TextStyle(
-            fontSize: 10,
-            color: primeCOLOR,
+    return Row(
+      children: [
+        Container(
+          // width: 70,
+          // height: 23,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+                topRight: Radius.circular(10),
+                bottomRight: Radius.circular(10)),
+            color: borderColor,
           ),
-          textAlign: TextAlign.center,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              Strings.installment,
+              style: TextStyle(
+                fontSize: 10,
+                color: primeCOLOR,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
         ),
-      ),
+      ],
     );
   }
 }
