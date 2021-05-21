1531dde99c95ebe41c3a14e26abb7c87d512d996
HamzaShakhshir <hamzash1122@outlook.com>
2021-05-21 14:39:05 +0300

    add drop list

--- lib/add_page/search_brand.dart
+++ lib/add_page/search_brand.dart
@@ -1,21 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:souqy/add_page/souqy_search_filed.dart';
 import 'package:souqy/res/car.dart';
+import 'package:souqy/res/exh.dart';
+import 'package:souqy/res/exhibitions.dart';
+import 'package:souqy/res/string.dart';
 import 'package:souqy/res/style.dart';
-
-import '../res/color.dart';
+import 'package:souqy/seller_pages/seller_page.dart';
 
 // ignore: must_be_immutable
 class SouqySearchForBrand extends StatefulWidget {
   final TextEditingController controller;
   final FocusNode focusNode;
+  final bool isVisibil;
+  final String selection;
+  final String choose;
   String count = '';
+  final void Function(String) onChangeSearch;
   List<String> onchangeList = [];
-  SouqySearchForBrand(
-      {Key key, @required this.controller, @required this.focusNode})
-      : super(key: key) {
+  List<Exh> exhibitionsList = [];
+  SouqySearchForBrand({
+    Key key,
+    @required this.controller,
+    @required this.focusNode,
+    this.onChangeSearch,
+    this.isVisibil = false,
+    this.selection,
+    this.choose,
+  }) : super(key: key) {
     onchangeList = brands;
     controller.text = count;
+    exhibitionsList = exhibitions;
   }
   @override
   State<StatefulWidget> createState() => _SouqySearchForBrandState();
@@ -32,6 +46,7 @@ class _SouqySearchForBrandState extends State<SouqySearchForBrand>
       query = '${query[0].toUpperCase()}${query.substring(1)}';
       widget.controller.text = query;
     }
+
     final se = brands.where((element) {
       final queryLower = query.toLowerCase();
       final elementLower = element.toLowerCase();
@@ -45,10 +60,30 @@ class _SouqySearchForBrandState extends State<SouqySearchForBrand>
     });
   }
 
+  void filterExhibitions(String query) {
+    if (query.isNotEmpty) {
+      query = '${query[0].toUpperCase()}${query.substring(1)}';
+    }
+
+    final se = exhibitions.where((element) {
+      final queryLower = query.toLowerCase();
+      final elementLower = element.getName().toLowerCase();
+      final cityLower = element.getCity().toLowerCase();
+      if (elementLower.contains(queryLower))
+        return elementLower.contains(queryLower);
+      else
+        return cityLower.contains(queryLower);
+    }).toList();
+
+    setState(() {
+      widget.count = query;
+      widget.exhibitionsList = se;
+    });
+  }
+
   void lossFocus() {
     if (widget.onchangeList.length == 1) {
       widget.count = widget.onchangeList[0];
-      widget.controller.text = widget.onchangeList[0];
     }
   }
 
@@ -60,62 +95,76 @@ class _SouqySearchForBrandState extends State<SouqySearchForBrand>
     return Column(children: [
       SouqySearchFiled(
         onChange: filterBrand,
+        onChangeSearch: widget.onChangeSearch,
+        onChangeExhibitions: filterExhibitions,
         controller: widget.controller,
         focusNode: widget.focusNode,
         lossFocus: lossFocus,
+        isVisibil: widget.isVisibil,
+        chosen: widget.choose,
       ),
-      Container(
-        height: (widget.onchangeList.length < 1) ? 0 : 100,
-        child: ListView.builder(
-            shrinkWrap: true,
-            scrollDirection: Axis.horizontal,
-            itemCount: getlength(),
-            itemBuilder: (context, index) {
-              MediaQuery.of(context).orientation == Orientation.portrait
-                  ? wid = size.width / 4
-                  : wid = size.width / 7;
-              return new GestureDetector(
-                child: new Container(
-                  margin: EdgeInsets.all(5),
-                  width: wid,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(32),
-                      boxShadow: [
-                        BoxShadow(
-                          // spreadRadius: 3,
-                          blurRadius: 3,
-                          color: Colors.black26,
-                        )
-                      ]),
-                  child: Center(
-                    child: Image.asset(
-                      'images/carBrand/' + widget.onchangeList[index] + '.png',
-                      width: 75,
-                      height: 75,
-                    ),
-                  ),
-                ),
-                onTap: () {
-                  setState(() {
-                    if (widget.onchangeList.length <= 1) {
-                      widget.count = "";
-                      widget.controller.text = widget.count;
-                      widget.onchangeList = brands;
-                    } else {
-                      widget.count = widget.onchangeList[index].toString();
-                      widget.controller.text = widget.count;
-                      widget.onchangeList = [];
-                      widget.onchangeList.add(widget.count);
-                      widget.controller.selection =
-                          returnToLast(widget.controller);
-                    }
-                  });
-                },
-              );
-            }),
-      ),
+      widget.choose == searchFilter[0] || widget.choose == null
+          ? Container(
+              height: (widget.onchangeList.length < 1) ? 0 : 100,
+              child: ListView.builder(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: getlength(),
+                  itemBuilder: (context, index) {
+                    MediaQuery.of(context).orientation == Orientation.portrait
+                        ? wid = size.width / 4
+                        : wid = size.width / 7;
+                    return new GestureDetector(
+                      child: new Container(
+                        margin: EdgeInsets.all(5),
+                        width: wid,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(32),
+                            boxShadow: [
+                              BoxShadow(
+                                // spreadRadius: 3,
+                                blurRadius: 3,
+                                color: Colors.black26,
+                              )
+                            ]),
+                        child: Center(
+                          child: Image.asset(
+                            'images/carBrand/' +
+                                widget.onchangeList[index] +
+                                '.png',
+                            width: 75,
+                            height: 75,
+                          ),
+                        ),
+                      ),
+                      onTap: () {
+                        setState(() {
+                          if (widget.onchangeList.length <= 1) {
+                            widget.count = "";
+                            widget.controller.text = widget.count;
+                            widget.onchangeList = brands;
+                          } else {
+                            widget.count =
+                                widget.onchangeList[index].toString();
+                            widget.controller.text = widget.count;
+                            widget.onchangeList = [];
+                            widget.onchangeList.add(widget.count);
+                            widget.controller.selection =
+                                returnToLast(widget.controller);
+                          }
+                        });
+                      },
+                    );
+                  }),
+            )
+          : Container(
+              height: MediaQuery.of(context).size.height * .67,
+              child: SouqySellerList(
+                exhibitionsList: widget.exhibitionsList,
+              ),
+            ),
     ]);
   }
 }
--- lib/add_page/souqy_search_filed.dart
+++ lib/add_page/souqy_search_filed.dart
@@ -1,60 +1,104 @@
 import 'package:flutter/material.dart';
 import 'package:souqy/res/car.dart';
+import 'package:souqy/res/dropDownList.dart';
 import 'package:souqy/res/string.dart';
 import 'package:wc_form_validators/wc_form_validators.dart';
-
 import '../res/color.dart';
 import '../res/style.dart';
 
 class SouqySearchFiled extends StatefulWidget with SouqyFormFieldStyle {
   final void Function(String) onChange;
+  final void Function(String) onChangeExhibitions;
+  final void Function(String) onChangeSearch;
+  final chosen;
   final FocusNode focusNode;
   final controller;
   final VoidCallback lossFocus;
+  final bool isVisibil;
   SouqySearchFiled({
     key,
     this.onChange,
+    this.onChangeExhibitions,
+    this.onChangeSearch,
     this.controller,
     @required this.focusNode,
     this.lossFocus,
+    this.isVisibil = false,
+    this.chosen,
   }) : super(key: key);
 
   @override
   _SouqySearchFiledState createState() => _SouqySearchFiledState();
 }
 
+var flex;
+
 class _SouqySearchFiledState extends State<SouqySearchFiled> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      // height: 90,
-      constraints: BoxConstraints(minHeight: 70),
-      padding: EdgeInsets.symmetric(horizontal: 17, vertical: 16),
-      child: Focus(
-        onFocusChange: (focus) {
-          if (focus == false) {
-            widget.lossFocus();
-          }
-        },
-        child: TextFormField(
-          controller: widget.controller,
-          focusNode: widget.focusNode,
-          textInputAction: TextInputAction.next,
-          cursorColor: fontColor,
-          style: TextStyle(fontSize: 17, color: fontColor),
-          decoration: widget.searchDecoration,
-          autovalidateMode: AutovalidateMode.onUserInteraction,
-          validator: (String value) {
-            if (Validators.required(Strings.requiredField(Strings.make)) ==
-                null) return Strings.requiredField(Strings.make);
-            if (!brands.contains(value)) return "must be a brand";
-            return null;
-          },
-          onChanged: (value) {
-            widget.onChange(value);
-          },
-        ),
-      ),
-    );
+    MediaQuery.of(context).orientation == Orientation.portrait
+        ? flex = 6
+        : flex = 10;
+    return Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Flexible(
+            flex: flex,
+            child: Container(
+              // height: 90,
+              constraints: BoxConstraints(minHeight: 70),
+              padding: EdgeInsets.symmetric(horizontal: 17, vertical: 16),
+              child: Focus(
+                  onFocusChange: (focus) {
+                    if (focus == false) {
+                      widget.lossFocus();
+                    }
+                  },
+                  child: widget.chosen == searchFilter[0] ||
+                          widget.chosen == null
+                      ? TextFormField(
+                          controller: widget.controller,
+                          focusNode: widget.focusNode,
+                          textInputAction: TextInputAction.next,
+                          cursorColor: fontColor,
+                          style: TextStyle(fontSize: 17, color: fontColor),
+                          decoration: widget.searchDecoration,
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          validator: (String value) {
+                            if (Validators.required(
+                                    Strings.requiredField(Strings.make)) ==
+                                null)
+                              return Strings.requiredField(Strings.make);
+                            if (!brands.contains(value))
+                              return "must be a brand";
+                            return null;
+                          },
+                          onChanged: (value) {
+                            widget.onChange(value);
+                          },
+                        )
+                      : TextFormField(
+                          controller: widget.controller,
+                          textInputAction: TextInputAction.done,
+                          cursorColor: fontColor,
+                          style: TextStyle(fontSize: 17, color: fontColor),
+                          decoration: widget.searchDecorationExhibition,
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          onChanged: (value) {
+                            widget.onChangeExhibitions(value);
+                          },
+                        )),
+            ),
+          ),
+          widget.isVisibil == true
+              ? Flexible(
+                  child: DrobDownList(
+                    listOfThings: searchFilter,
+                    selected: widget.onChangeSearch,
+                  ),
+                )
+              : Flexible(flex: 0, child: Container()),
+        ]);
   }
 }
