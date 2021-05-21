ffe6d7712daf31c86ab3727f83b8b8db97039ab8
HamzaShakhshir <hamzash1122@outlook.com>
2021-05-21 14:26:18 +0300

    create exhibitions list just for example

---
+++ lib/res/exhibitions.dart
@@ -0,0 +1,16 @@
+import 'exh.dart';
+
+List<Exh> exhibitions = [
+  Exh('Hamza0', 'Nablus', 'Fatauer st'),
+  Exh('Akram0', 'Ramala', 'Alersam st'),
+  Exh('Nasser0', 'Qalqilya', 'Big st'),
+  Exh('Hamza1', 'Nablus', 'Fatauer st'),
+  Exh('Akram1', 'Nablus', 'Alersam st'),
+  Exh('Nasser1', 'Qalqilya', 'Big st'),
+  Exh('Hamza2', 'Nablus', 'Fatauer st'),
+  Exh('Akram2', 'Ramala', 'Alersam st'),
+  Exh('Nasser2', 'Qalqilya', 'Big st'),
+  Exh('Hamza3', 'Nablus', 'Fatauer st'),
+  Exh('Akram3', 'Ramala', 'Alersam st'),
+  Exh('Nasser3', 'Nablus', 'Big st'),
+];
