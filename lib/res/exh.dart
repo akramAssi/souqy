3097ac3ea4bcbc4ded9839c054619533c70b1457
HamzaShakhshir <hamzash1122@outlook.com>
2021-05-21 14:25:43 +0300

    create Exh class

---
+++ lib/res/exh.dart
@@ -0,0 +1,19 @@
+class Exh {
+  String _name, _city, _str;
+  Exh(name, city, str) {
+    this._name = name;
+    this._city = city;
+    this._str = str;
+  }
+  String getName() {
+    return this._name;
+  }
+
+  String getCity() {
+    return this._city;
+  }
+
+  String getStr() {
+    return this._str;
+  }
+}
