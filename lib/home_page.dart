f06f8a8224cfd8d2c490e8a55b76096a1930718b
HamzaShakhshir <hamzash1122@outlook.com>
2021-05-21 14:35:19 +0300

    add StatisticPage

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:souqy/search_page/search.dart';
-import 'package:souqy/seller_pages/seller_page.dart';
 import 'package:souqy/home_pages/souqy_home_page.dart';
 import 'package:souqy/res/color.dart';
 import 'package:souqy/res/string.dart';
 import 'package:souqy/service/locator.dart';
+import 'package:souqy/statistic/statistic_page.dart';
 import 'package:souqy/trash/1.dart';
 import 'package:souqy/view_controller/user_controller.dart';
 import 'package:souqy/widget/showExceptionDilog.dart';
@@ -121,12 +121,7 @@ class _HomePageState extends State<HomePage> {
     );
 
     _widgetOptions.add(Container(
-      child: TextButton(
-        child: Text("sign out"),
-        onPressed: () {
-          _singOut();
-        },
-      ),
+      child: StatisticPage(),
     ));
     return ss;
   }
